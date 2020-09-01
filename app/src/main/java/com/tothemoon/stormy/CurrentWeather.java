3d9c078f8b6b2f698cedb81bddd4ae16e13c97ea
Nathan Good <goodlnath@gmail.com>
2020-09-01 21:54:39 +0100

    1

---
+++ app/src/main/java/com/tothemoon/stormy/CurrentWeather.java
@@ -0,0 +1,69 @@
+package com.tothemoon.stormy;
+
+public class CurrentWeather {
+
+    private String locationLabel;
+    private String icon;
+    private long time;
+
+    public String getLocationLabel() {
+        return locationLabel;
+    }
+
+    public void setLocationLabel(String locationLabel) {
+        this.locationLabel = locationLabel;
+    }
+
+    public String getIcon() {
+        return icon;
+    }
+
+    public void setIcon(String icon) {
+        this.icon = icon;
+    }
+
+    public long getTime() {
+        return time;
+    }
+
+    public void setTime(long time) {
+        this.time = time;
+    }
+
+    public double getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(double temperature) {
+        this.temperature = temperature;
+    }
+
+    public double getHumidity() {
+        return humidity;
+    }
+
+    public void setHumidity(double humidity) {
+        this.humidity = humidity;
+    }
+
+    public double getPrecipChance() {
+        return precipChance;
+    }
+
+    public void setPrecipChance(double precipChance) {
+        this.precipChance = precipChance;
+    }
+
+    public String getSummary() {
+        return summary;
+    }
+
+    public void setSummary(String summary) {
+        this.summary = summary;
+    }
+
+    private double temperature;
+    private double humidity;
+    private double precipChance;
+    private String summary;
+}
